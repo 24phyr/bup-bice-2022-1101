18125aea79b1f243d2cab9ca5280e51eb0721463
Fardin Islam <31590240+24phyr@users.noreply.github.com>
2022-10-18 23:50:02 +0600

    Create hello.c

---
+++ ICE1101/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>                   // Header file contains the function declarations
+int main()                          // main function
+{
+    printf("hello, world\n");       // Displays Hello, World
+
+    return 0;
+}

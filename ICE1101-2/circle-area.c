28a5895340b859112d95fb83b12dc28b276f3b3f
Fardin Islam <31590240+24phyr@users.noreply.github.com>
2022-10-29 10:50:40 +0600

    Update circle-area.c

--- ICE1101-2/circle-area.c
+++ ICE1101-2/circle-area.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <math.h>                                       //used for the pow function in line 13
-#define Pi 3.1416                                       //Pi is a constant. So we used #define to declare it
+#include <math.h>                                       // Used for the pow function in line 13
+#define Pi 3.1416                                       // Pi is a constant. So we used #define to declare it
 
 int main()
 {
@@ -12,7 +12,7 @@ int main()
 
     R2=pow(Radius,2);
 
-    Area=Pi*R2;                                         //formula of finding the area of a circle
+    Area=Pi*R2;                                         // Formula of finding the area of a circle
 
     printf("Area of the Circle:""%.2f", Area);
 

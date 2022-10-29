2ec6dac28ae28a08d09f78b46476bb676a69952a
Fardin Islam <31590240+24phyr@users.noreply.github.com>
2022-10-29 16:26:25 +0600

    Create array-reverse.c

---
+++ ICE1101-2/array-reverse.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n, i;
+
+    printf("Enter the size of array a:");
+    scanf("%d" , &n);
+
+    int a[n];
+    printf("Enter the elements of Array a:\n");
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d" , &a[i]);
+    }
+
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%d\t", a[i]);
+    }
+    return 0;
+}

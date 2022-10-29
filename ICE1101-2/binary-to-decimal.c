e87f5a0fe0c9eed84e3c6dc9d1f08e29890336b4
Fardin Islam <31590240+24phyr@users.noreply.github.com>
2022-10-30 02:13:35 +0600

    Update binary-to-decimal.c

--- ICE1101-2/binary-to-decimal.c
+++ ICE1101-2/binary-to-decimal.c
@@ -5,19 +5,19 @@ int main()
 {
     int binary , decimal , rem , i=0 , b;
 
-    printf("Enter the Binary number:");
+    printf("Enter the Binary number: ");
     scanf("%d" , &binary);
 
     while(binary!=0)
     {
         rem=binary % 10;
-        binary /= 10;
         b=pow(2,i);
         decimal += rem*b;
+        binary /= 10;
         i++;
     }
 
-    printf("%d" , decimal);
+    printf("Decimal value: %d" , decimal);
 
     return 0;
 }

bc9ab643bb0b2bb115e08a43444cdbb3ef6e23a3
Fardin Islam <31590240+24phyr@users.noreply.github.com>
2022-10-29 10:37:52 +0600

    Create right-angle-triangle.c

---
+++ ICE1101-2/right-angle-triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int base, perpendicular;
+    float Area;
+
+    base=4;
+    perpendicular=3;
+
+    Area=(base*perpendicular)/2;                //formula to get the area of right angle triangle
+
+    printf("Area of the Triangle:""%f", Area);
+
+    return 0;
+}

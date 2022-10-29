937cc4c5c7b05d5b94c8dc190b2f76aea89fbfa6
Fardin Islam <31590240+24phyr@users.noreply.github.com>
2022-10-29 10:46:06 +0600

    Create circle-area.c

---
+++ ICE1101-2/circle-area.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>                                       //used for the pow function in line 13
+#define Pi 3.1416                                       //Pi is a constant. So we used #define to declare it
+
+int main()
+{
+    int Radius, R2;
+    float Area;
+
+    printf("Enter the Radius value of the circle:");
+    scanf("%d", &Radius);
+
+    R2=pow(Radius,2);
+
+    Area=Pi*R2;                                         //formula of finding the area of a circle
+
+    printf("Area of the Circle:""%.2f", Area);
+
+    return 0;
+}
